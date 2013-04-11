ef452afb5854de24ba1952d640b9a11e6953036f
smcm1000 <smcm1000@gmail.com>
2013-04-11 13:02:04 +0100

    Initial commit

---
+++ OOD-MyCalculator/src/ie/gcd/myCalculator/Calculator.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package ie.gcd.myCalculator;
+
+/**
+ * @author seamusmcmorris
+ *
+ */
+public class Calculator {
+
+	/**
+	 * 
+	 */
+	public Calculator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ OOD-MyCalculator/src/ie/gcd/tests/CalculatorTest.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package ie.gcd.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author seamusmcmorris
+ *
+ */
+public class CalculatorTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
