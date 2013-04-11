4ab5d5060c60c2670a8739930d4e991776c86b66
smcm1000 <smcm1000@gmail.com>
2013-04-11 13:20:47 +0100

    Branch Setup

---
+++ OOD-MyCalculator/src/ie/gcd/myCalculator/ScientificCalculator.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package ie.gcd.myCalculator;
+
+/**
+ * @author seamusmcmorris
+ *
+ */
+public class ScientificCalculator {
+
+	/**
+	 * 
+	 */
+	public ScientificCalculator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int add(int x, int y){
+		return x-y;
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ OOD-MyCalculator/src/ie/gcd/tests/ScientificCalculatorTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package ie.gcd.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author seamusmcmorris
+ *
+ */
+public class ScientificCalculatorTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link ie.gcd.myCalculator.ScientificCalculator#add(int, int)}.
+	 */
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+}
